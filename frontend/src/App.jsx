bccd58d3749f27722a52863d33a40a0fbba70715
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-05-26 08:45:23 +0545

    done with the navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
-
+import { Routes, Route } from 'react-router'
 import './App.css'
+import Home from './pages/Home.jsx'
+import CreateNote from './pages/CreateNote.jsx'
+import NoteDetailPage from './pages/NoteDetailPage.jsx'
 
 function App() {
 
   return (
-    <>
-      <h1 className='text-blue-500'>Hello</h1>
-      <button className="btn btn-primary">Button</button>
-    </>
+    <div data-theme="forest">
+      <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/create' element={<CreateNote/>} />
+          <Route path='/note/:id' element={<NoteDetailPage/>} />
+      </Routes>
+    </div>
   )
 }
 
