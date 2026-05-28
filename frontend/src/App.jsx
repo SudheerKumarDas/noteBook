74dfdd55669dc2ba2e68ff5005e37c7c80d7387e
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-05-28 20:04:56 +0545

    created a form to take inputs like title and content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import Home from './pages/Home.jsx'
 import CreateNote from './pages/CreateNote.jsx'
 import NoteDetailPage from './pages/NoteDetailPage.jsx'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
   return (
     <div data-theme="forest">
+      <Toaster/>
       <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/create' element={<CreateNote/>} />
--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -5,7 +5,7 @@ import { formatDate } from "../lib/utils.js"
 function NoteCard({note}) {
   return (
     <Link to={`note/${note._id}`}
-        className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D]"
+        className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-2 border-solid border-[#00FF9D]"
     >
         <div className="card-body">
             <h3 className="card-title text-base-content">{note.title}</h3>
