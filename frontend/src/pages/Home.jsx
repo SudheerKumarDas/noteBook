a2a196749f7c226e6df8d11ea8f24ff8a0f29041
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-05-28 18:09:45 +0545

    created noteUi

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar.jsx"
 import RateLimiterUI from "../components/RateLimiterUI.jsx"
 import { useEffect, useState } from "react"
 import toast from "react-hot-toast"
+import NoteCard from "../components/NoteCard.jsx";
 
 function Home() {
     const [isRateLimited, setIsRateLimited]= useState(false);
@@ -34,6 +35,21 @@ function Home() {
     <div className="min-h-screen">
         <Navbar/>
         {isRateLimited && <RateLimiterUI/>}
+
+        <div className="max-w-7xl mx-auto p-4 mt-6">
+            {loading && <div className="text-center text-primary py-10"> loading...</div> }
+
+            {notes.length > 0 && !isRateLimited && (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {notes.map((note)=>(
+                            <div>
+                                <NoteCard key={note._id} note={note}/>
+                            </div>
+                        )
+                    )}
+                </div>
+            )}
+        </div>
     </div>
   )
 }
